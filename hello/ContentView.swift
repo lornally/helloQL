c90e971fb2c255c90a43187b7f7d7e00dc8272d7
bergman <2024.06.27ventura@mac.studio.2022>
2025-08-23 11:48:23 +0800

    Initial Commit

---
+++ hello/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hello
+//
+//  Created by 驺虞茂弘 on 2025/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hello/helloApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloApp.swift
+//  hello
+//
+//  Created by 驺虞茂弘 on 2025/8/23.
+//
+
+import SwiftUI
+
+@main
+struct helloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
